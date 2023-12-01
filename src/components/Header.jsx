979baa7a68df9e1c985a80b10fe8118fe7f71c1c
Farina03 <tahsinafarina@gmail.com>
2023-12-01 13:01:00 +0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,19 @@ const Header = () => {
       <div className='header-div'>
         <h1>Registration Form</h1>
         <p className='subtitle'>Please fill out this form with the required information</p>
-        <div className='circle-div'>
-            <div className='circle1'><p>1</p></div>
-            <hr></hr>
-            <div className='circle2'><p>2</p></div>
-            <hr></hr>
-            <div className='circle3'><p>3</p></div>
+        <div className='graphics-div'>
+            <div className='circle-div'>
+              <div className='circle1'><p>1</p></div>
+              <hr></hr>
+              <div className='circle2'><p>2</p></div>
+              <hr></hr>
+              <div className='circle3'><p>3</p></div>
+            </div>
+            <div className='phase-name-div'>
+              <div className='phase'>Personal Information</div>
+              <div className='phase'>Edicational Information</div>
+              <div className='phase'>Experience Information</div>
+            </div>
         </div>
     </div>
   )
